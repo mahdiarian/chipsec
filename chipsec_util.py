6cfbc1ecd1b7b814dfa7c2fe7141d9cca248c382
Mahdi Mohajeri <mohajerimahdi86@gmail.com>
2016-12-21 13:54:02 +0330

    Update chipsec_util.py
    Updates to contact information

--- chipsec_util.py
+++ chipsec_util.py
@@ -13,10 +13,10 @@
 #
 #You should have received a copy of the GNU General Public License
 #along with this program; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#Foundation, CETEC, Number 9, HosseinAli alley, Farjam Street, First Floor, Tehran, Islamic Republic of Iran.
 #
 #Contact information:
-#chipsec@intel.com
+#mahdi.mohajeri@ut.ac.ir
 #
 
 
